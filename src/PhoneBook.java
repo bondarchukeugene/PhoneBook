330599208520a3395d94d2e257cdab865a507efe
eugenebondarchuk <bondarchuk.eugene@gmial.com>
2020-09-04 15:17:44 +0300

    Add mrthod check name length

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -1,14 +1,27 @@
+import java.util.Scanner;
+
 public class PhoneBook {
 
     public static void main(String[] args) {
         //Добавить считывание ввода пользователя в цикле
+        boolean continueToWork = true;
+        while (continueToWork) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Give mne a name");
+            String name = scanner.nextLine();
+            Boolean isNameCorrect = PhoneBook.checkName(name);
+            System.out.println(isNameCorrect);
+            continueToWork = false;
+        }
+    }
+
+
+    public static boolean checkName(String name) {
+        int length = name.trim().split(" ").length;
+        return length == 3;
     }
 
     public static boolean checkPhoneNumber(String phoneNumber) {
-        return true;
-    }
-
-    public static boolean checkName(String name) {
         return true;
     }
 
@@ -26,5 +39,8 @@ public class PhoneBook {
 
     public static void list(String[][] book) {
 //        test changces
+
+
     }
 }
+
